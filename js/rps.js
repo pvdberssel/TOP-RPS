4c5fae34178b399a268b8df13d9b0fe064ab950b
pvdberssel <pim.bersselaar@live.com>
2024-10-07 14:05:21 +0200

    Add choice AI game

--- js/rps.js
+++ js/rps.js
@@ -1,1 +1,25 @@
 console.log("Hello World!")
+
+
+function getComputerChoice(){
+    let max = 3;
+    switch(getRandomInt(max)){
+        case 0:
+            choice = 'Rock';
+            break;
+        case 1:
+            choice = 'Paper';
+            break;
+        case 2:
+            choice = 'Scissors';
+            break;
+    }
+    
+    return choice;
+}
+
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
+}
+
+console.log(getComputerChoice())
