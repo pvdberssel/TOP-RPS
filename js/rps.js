b17cdd50320c59ac1c8694fc2f6d8b3a77472102
pvdberssel <pim.bersselaar@live.com>
2024-10-07 14:19:15 +0200

    Add Game logic

--- js/rps.js
+++ js/rps.js
@@ -1,17 +1,53 @@
 console.log("Hello World!")
 
 
+let humanScore = 0;
+let computerScore = 0;
+
+
+playRound();
+
+function playRound(){
+
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+
+    console.log(humanChoice);
+    console.log(computerChoice);
+
+    if (humanChoice === computerChoice){
+        console.log('Draw')
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors'){
+        console.log('You win');
+        humanScore += 1;
+    } else if (humanChoice == 'paper' && computerChoice == 'rock'){
+        console.log('You win');
+        humanScore += 1;
+    }
+    else if (humanChoice == 'scissor' && computerChoice == 'paper'){
+        console.log('You win');
+        humanScore += 1;
+    } else {
+        console.log('You lose');
+        computerScore += 1;
+    }
+}
+
+
+
+
+
 function getComputerChoice(){
     let max = 3;
     switch(getRandomInt(max)){
         case 0:
-            choice = 'Rock';
+            choice = 'rock';
             break;
         case 1:
-            choice = 'Paper';
+            choice = 'paper';
             break;
         case 2:
-            choice = 'Scissors';
+            choice = 'scissors';
             break;
     }
     
@@ -27,12 +63,9 @@ function getHumanChoice(){
     choice = choice.toLowerCase();
 
     while(!(choice == 'rock' || choice == 'paper' || choice == 'scissors')){
-        console.log('Not a correct option')
+        console.log('Try again, Input invalid')
         choice = prompt("Rock, Paper or Scissors?");
         choice = choice.toLowerCase();
     }
     return choice.toLowerCase();
 }
-
-console.log(getComputerChoice())
-console.log(getHumanChoice())
